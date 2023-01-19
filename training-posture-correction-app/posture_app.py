fcdd51e4f268738839dbd4a9066d0aabd5df919f
ha-zu <nagaoka0132@gmail.com>
2023-01-19 21:16:35 +0900

    fix typo.

--- training-posture-correction-app/posture_app.py
+++ training-posture-correction-app/posture_app.py
@@ -88,7 +88,7 @@ def recording_format()->Any:
 
     fmt = cv.VideoWriter_fourcc("m", "p", "4", "v")
     video_out = os.path.join(os.getcwd(), cl.VIDEO_OUT_DIR, '{}.mp4'.format(uuid.uuid1()))
-    writer = cv.VideoWriter(video_out, fmt, cl.VIDEO_FRAME_RATE, (cl.VIDEO_FRAME_WIDTH, cl.VIDEO_FRAME_HEIGHT))
+    writer = cv.VideoWriter(video_out, fmt, cl.VIDEO_FRAME_RATE10, (cl.VIDEO_FRAME_WIDTH, cl.VIDEO_FRAME_HEIGHT))
 
     return writer
 
